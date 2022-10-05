dfbcae9686df370358adbc060b898db1b50a2896
JakubFaltyn <jakubfaltyn@gmail.com>
2022-10-05 20:41:08 +0200

    Create main.js

---
+++ EX, 10/main.js
@@ -0,0 +1,13 @@
+const dog = {
+  name: "Hachiko",
+  size: "Big",
+  color: "brown",
+
+  giveVoice() {
+    console.log("Hau Hau");
+  },
+
+  getDog() {
+    return dog.name + dog.size + dog.color;
+  },
+};
